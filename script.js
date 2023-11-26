83c9b26ec08cee6b669958427d79d0da597a4d1c
Julian Rahimi <julianrahimi@Air-von-Julian.lan>
2023-11-26 03:38:31 -0800

    more adjustments

--- script.js
+++ script.js
@@ -1,44 +1,25 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-item');
-const totalSlides = slides.length;
-let slideInterval = setInterval(moveToNextSlide, 3000);
+document.addEventListener('DOMContentLoaded', (event) => {
+    let currentActiveIndex = 0;
+    const slides = document.querySelectorAll('.carousel-item');
+    const numberOfSlides = slides.length;
 
-document.querySelector('.left-arrow').addEventListener('click', function() {
-    moveToPrevSlide();
-    restartSlideInterval();
+    // Function to update the slide display
+    function updateSlideDisplay() {
+        slides.forEach((slide, index) => {
+            slide.style.display = 'none'; // Hide all slides
+        });
+        slides[currentActiveIndex].style.display = 'block'; // Show the current slide
+    }
+
+    // Function to go to the next slide
+    function goToNextSlide() {
+        currentActiveIndex = (currentActiveIndex + 1) % numberOfSlides; // Cycle back to first slide after the last
+        updateSlideDisplay();
+    }
+
+    // Initial update of the slide display
+    updateSlideDisplay();
+
+    // Set an interval to automatically go to the next slide
+    setInterval(goToNextSlide, 3000); // Change slides every 3 seconds
 });
-
-document.querySelector('.right-arrow').addEventListener('click', function() {
-    moveToNextSlide();
-    restartSlideInterval();
-});
-
-function updateSlidePosition() {
-    for (let slide of slides) {
-        slide.classList.remove('active');
-    }
-    slides[currentSlide].classList.add('active');
-}
-
-function moveToPrevSlide() {
-    if (currentSlide === 0) {
-        currentSlide = totalSlides - 1;
-    } else {
-        currentSlide--;
-    }
-    updateSlidePosition();
-}
-
-function moveToNextSlide() {
-    if (currentSlide === totalSlides - 1) {
-        currentSlide = 0;
-    } else {
-        currentSlide++;
-    }
-    updateSlidePosition();
-}
-
-function restartSlideInterval() {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(moveToNextSlide, 3000);
-}
